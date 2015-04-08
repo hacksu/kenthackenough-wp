c1a10779012df552c26589f5d71237a04ee73d0b
bwarring24 <bwarring24@gmail.com>
2015-04-08 16:11:25 -0400

    Included missing files that were not being controlled.

---
+++ Kent Hack Enough/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Kent_Hack_Enough.Resources;
+
+namespace Kent_Hack_Enough
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
