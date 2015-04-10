f570426123a3782eb22ed40c048caf87ed951d11
bwarring24 <bwarring24@gmail.com>
2015-04-10 12:06:50 -0400

    Added SocketRequest class

---
+++ Kent Hack Enough/SocketRequest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kent_Hack_Enough
+{
+    class SocketRequest
+    {
+            public SocketRequest()
+            {
+                nsp = null;
+                cmd = null;
+                data = null;
+            }
+
+            public SocketRequest(string nspTmp, string cmdTmp, string dataTmp)
+            {
+                nsp = nspTmp;
+                cmd = cmdTmp;
+                data = dataTmp;
+            }
+
+            public string getNsp()
+            {
+                return nsp;
+            }
+
+            public string getCmd()
+            {
+                return cmd;
+            }
+
+            public string getData()
+            {
+                return data;
+            }
+
+            private string nsp;
+            private string cmd;
+            private string data;
+        }
+}
