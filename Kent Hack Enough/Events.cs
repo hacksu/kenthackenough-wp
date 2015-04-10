7b33e21466a3961967cfd42c30ca88a554105243
bwarring24 <bwarring24@gmail.com>
2015-04-10 12:12:45 -0400

    Wrote class Events and class RootEvents

--- Kent Hack Enough/Events.cs
+++ Kent Hack Enough/Events.cs
@@ -8,5 +8,16 @@ namespace Kent_Hack_Enough
 {
     class Events
     {
+        public string _id { get; set; }
+        public string name { get; set; }
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
+        public string group { get; set; }
+        public bool notify { get; set; }
+    }
+
+    class RootEvents
+    {
+        public List<Events> events { get; set; }
     }
 }
