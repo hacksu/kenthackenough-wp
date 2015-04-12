86d6c0c5df84a61df0f0059ebae81d1c5ba46d12
bwarring24 <bwarring24@gmail.com>
2015-04-12 18:55:56 -0400

    Displays the elements to the scroll view

--- Kent Hack Enough/Events.cs
+++ Kent Hack Enough/Events.cs
@@ -1,8 +1,16 @@
-﻿using System;
+﻿using Microsoft.Phone.Controls;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Windows.UI.Core;
 
 namespace Kent_Hack_Enough
 {
@@ -20,4 +28,186 @@ namespace Kent_Hack_Enough
     {
         public List<Events> events { get; set; }
     }
+
+     public class Event
+    {
+        const string API_SERVER = "http://api.khe.pdilyard.com/v1.0/";
+        HttpWebRequest request;
+        System.Threading.Timer Timer;
+        private AppSettings settings = new AppSettings();
+        
+        
+        
+        public Event() { }
+
+        Event(string eventId, string eventName, DateTime eventStart, DateTime eventEnd, String eventGroup, bool eventNotify)
+        {
+            RootEvents rootEvent = new RootEvents();
+            Events events = new Events();
+            
+            events._id = eventId;
+            events.name = eventName;
+            events.start = eventStart;
+            events.end = eventEnd;
+            events.group = eventGroup;
+            events.notify = eventNotify;
+
+            rootEvent.events.Add(events);
+        }
+
+        public void getEvent()
+        {
+            Timer = new Timer(TimerCallback, null, 0, Convert.ToInt16(settings.RefreshIntervalSetting) * 1000);
+        }
+
+        public TextBlock parseText(Events msg)
+        {
+            TextBlock result = new TextBlock();
+            Markdown md = new Markdown();
+
+            result = md.parseMarkdown(msg.name);
+
+            return result;
+        }
+
+        public DateTime parseDate(DateTime msg)
+        {
+            DateTime dt = Convert.ToDateTime(msg.ToString());
+            DateTimeOffset dateAndOffset = new DateTimeOffset(dt, TimeZoneInfo.Local.GetUtcOffset(dt));
+
+            return dateAndOffset.DateTime;
+        }
+
+
+        private void toggleProg()
+        {
+            Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                // Your UI update code goes here!
+                try
+                {
+                    MainPage main = (MainPage)((PhoneApplicationFrame)Application.Current.RootVisual).Content;
+                    if (main.progBar.Visibility == Visibility.Collapsed)
+                    {
+                        main.progBar.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
+                        main.progBar.Visibility = Visibility.Collapsed;
+                    }
+                }
+                catch (Exception)
+                {
+                    //   throw;
+                }
+            });
+        }
+
+
+        private void refreshEvents()
+        {
+            Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                try
+                {
+                    // Your UI update code goes here!
+                    MainPage main = (MainPage)((PhoneApplicationFrame)Application.Current.RootVisual).Content;
+                    main.LiveFeedItems.Children.Clear();
+                    int j = settings.EventsSetting.events.Count() - 1;
+
+                    for (int i = j; i >= 0; i--)
+                    {
+                        TextBlock txtMsg = new TextBlock();
+                        TextBlock txtStart = new TextBlock();
+                        TextBlock txtEnd = new TextBlock();
+                        StackPanel stkContainer = new StackPanel();
+
+
+                        stkContainer.Height = 100;
+                        stkContainer.Background = new SolidColorBrush(Color.FromArgb(125, 255, 0, 0));
+                        stkContainer.Margin = new System.Windows.Thickness(5.0);
+
+
+                        txtMsg = parseText(settings.EventsSetting.events[i]);
+                        txtMsg.Margin = new System.Windows.Thickness(5.0);
+
+                        txtStart.Text = parseDate(settings.EventsSetting.events[i].start).ToString();
+                        txtStart.Margin = new System.Windows.Thickness(5.0);
+                        txtStart.HorizontalAlignment = System.Windows.HorizontalAlignment.Right;
+                        txtStart.VerticalAlignment = System.Windows.VerticalAlignment.Bottom;
+
+                        txtEnd.Text = parseDate(settings.EventsSetting.events[i].start).ToString();
+                        txtEnd.Margin = new System.Windows.Thickness(5.0);
+                        txtEnd.HorizontalAlignment = System.Windows.HorizontalAlignment.Right;
+                        txtEnd.VerticalAlignment = System.Windows.VerticalAlignment.Bottom;
+
+                        stkContainer.Children.Add(txtMsg);
+                        stkContainer.Children.Add(txtStart);
+                        stkContainer.Children.Add(txtEnd);
+
+                        main.EventsItems.Children.Add(stkContainer);
+                    }
+                }
+                catch (Exception)
+                {
+                    //   throw;
+                }
+            });
+        }
+
+
+
+        #region WebCleint
+
+        void webClient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            try
+            {
+                string data = e.Result;
+
+                var results = JsonConvert.DeserializeObject<dynamic>(e.Result);
+
+                RootMessages Result = JsonConvert.DeserializeObject<RootMessages>(e.Result);
+
+                settings.LiveFeedSetting = Result;
+                settings.Save();
+
+                refreshEvents();
+            }
+            catch
+            {
+                return;
+            }
+
+            toggleProg();
+        }
+
+
+        private void TimerCallback(object state)
+        {
+            Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                // Your UI update code goes here!
+                try
+                {
+                    MainPage main = (MainPage)((PhoneApplicationFrame)Application.Current.RootVisual).Content;
+                    toggleProg();
+                }
+                catch (Exception)
+                {
+                    //throw;
+                }
+            });
+
+
+            WebClient webClient = new WebClient();
+
+            webClient.DownloadStringCompleted += new DownloadStringCompletedEventHandler(webClient_DownloadStringCompleted);
+
+            webClient.Headers[HttpRequestHeader.IfModifiedSince] = DateTime.UtcNow.ToString();
+
+            webClient.DownloadStringAsync(new Uri(API_SERVER + "/events"));
+        }
+
+        #endregion
 }
