e5e2b40e1290fe436c283aac5f862888ac1ea414
bwarring24 <bwarring24@gmail.com>
2015-04-12 18:40:13 -0400

    Removed un-needed namespace System.Windows.Documents

--- Kent Hack Enough/LiveFeed.cs
+++ Kent Hack Enough/LiveFeed.cs
@@ -9,7 +9,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
 using System.Windows.Media;
 using Windows.UI.Core;
 
