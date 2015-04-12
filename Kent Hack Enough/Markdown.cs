c0b6869da1903de87d9faaf031c286df80b85587
bwarring24 <bwarring24@gmail.com>
2015-04-12 12:51:02 -0400

    Parses ** * _ and __

---
+++ Kent Hack Enough/Markdown.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace Kent_Hack_Enough
+{
+    class Markdown
+    {
+        public TextBlock parseMarkdown(LiveFeedMessages msg)
+        {
+            TextBlock result = new TextBlock();
+            
+            string tmp;
+            
+            int i = 0;
+            int start = 0;
+            int end = 0;
+            bool def = true;
+
+
+
+            while (i < msg.text.Length)
+            {
+
+                try
+                {
+
+
+
+
+                    start = 0;
+                    end = 0;
+
+
+                    // Single asterisk or double asterisk
+                    if ((msg.text[i] == '*') && (msg.text[i + 1] != '*'))
+                    {
+                        start = index(msg.text, '*', i) + 1;
+                        end = index(msg.text, '*', (start + 1));
+                        tmp = msg.text.Substring(start, (end - start));
+                        // MessageBox.Show(end.ToString());
+
+                        Run textRun = new Run();
+                        textRun.Text = tmp + " ";
+                        textRun.FontStyle = FontStyles.Italic;
+                        result.Inlines.Add(textRun);
+
+                        i = end + 1;
+                        def = false;
+                    }
+                    // Single underline
+                    else if ((msg.text[i] == '_') && (msg.text[i + 1] != '_'))
+                    {
+                        start = msg.text.IndexOf('_', i - 1);
+                        end = msg.text.IndexOf('_', start);
+                        tmp = msg.text.Substring(start, end);
+                        Run textRun = new Run();
+                        textRun.Text = tmp + " ";
+                        textRun.FontStyle = FontStyles.Italic;
+                        result.Inlines.Add(textRun);
+
+                        i = end + 1;
+                        def = false;
+                    }
+
+
+
+                    // Double Asterisk or Double underscore
+                    if ((msg.text[i] == '*') && (msg.text[i + 1] == '*'))
+                    {
+                        start = msg.text.IndexOf('*', i) + 2;
+                        end = msg.text.IndexOf('*', start) - 2;
+                        tmp = msg.text.Substring(start, end);
+
+                        // Bold and italic
+                        if (tmp.Contains("_"))
+                        {
+                            string under;
+                            int cur2;
+
+                            cur2 = tmp.IndexOf('_', i) + 2;
+                            under = tmp.Substring(cur2, tmp.IndexOf('_', cur2));
+
+
+                            Run textRun = new Run();
+                            textRun.Text = tmp.Remove(cur2, under.Length) + " ";
+                            textRun.FontWeight = FontWeights.Bold;
+                            result.Inlines.Add(textRun);
+
+                            textRun = null;
+
+                            textRun.Text = under + " ";
+                            textRun.FontWeight = FontWeights.Bold;
+                            textRun.FontStyle = FontStyles.Italic;
+                            result.Inlines.Add(textRun);
+
+                            i = i + under.Length;
+                            def = false;
+                        }
+                        else
+                        {
+                            Run textRun = new Run();
+                            textRun.Text = tmp + " ";
+                            textRun.FontWeight = FontWeights.Bold;
+                            result.Inlines.Add(textRun);
+
+                            i = end + start + 3;
+                            def = false;
+                        }
+                    }
+                    // Double Underline
+                    else if ((msg.text[i] == '_') && (msg.text[i + 1] == '_'))
+                    {
+                        start = msg.text.IndexOf('_', i + 1);
+                        end = msg.text.IndexOf('_', start);
+                        tmp = msg.text.Substring(start, end);
+                        Run textRun = new Run();
+                        textRun.Text = tmp + " ";
+                        textRun.FontWeight = FontWeights.Bold;
+                        result.Inlines.Add(textRun);
+
+                        i = end + start + 1;
+                        def = false;
+                    }
+
+
+                  
+                } 
+                    
+                catch (Exception)
+                {
+                   // i++;
+                    //   throw;
+                }
+                i++;
+                
+            }
+            
+
+            if (def)
+                {
+                    result.Text = msg.text;
+                }
+            return result;
+        }
+
+        public int index(string txt, char s, int start)
+        {
+           int i = start;
+            //while(i != txt.Length)
+           // {
+                //string cur = txt.Substring(i, 1);
+               
+                if (txt.IndexOf(s, i) != -1)
+                {
+                    return txt.IndexOf(s, i);
+                }
+              
+                //i++;
+             
+           // }
+            return -1;
+        }
+    }
+
+
+}
